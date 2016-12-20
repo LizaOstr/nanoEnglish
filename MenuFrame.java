a0dfc3802f782f7c15c6a6aba69558fdc7e83b48
LizaOstr <antelizz@mail.ru>
2016-12-21 02:56:03 +0300

    Add files via upload
    Reading texts

---
+++ MenuFrame.java
@@ -0,0 +1,100 @@
+package nanoEnglish;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.*;
+
+public class MenuFrame extends JFrame{
+	
+	public MenuFrame() throws ClassNotFoundException{
+		setSize(550,730);
+		Container contentPane = getContentPane();
+		MenuPanel panel = new MenuPanel();
+		contentPane.add(panel);
+	}
+}
+
+class MenuPanel extends JPanel{
+	public static ReadingMaterialsFrame newframe;
+	Color RedOxide = new Color(82,21,21);
+	Color FaluRed = new Color(123,31,31);
+	Color CopperRose = new Color(157,111,111);
+	Color PetiteOrchid = new Color(210,149,149);
+	
+
+	public MenuPanel(){
+		setLayout(null);
+		setBackground(new Color(205,92,92));
+		
+		Font font = new Font("TimesRoman", Font.PLAIN, 28);
+		JLabel title = new JLabel("nanoEnglish");
+		title.setFont(font);
+		title.setForeground(Color.WHITE);
+		add(title).setBounds(200, 10, 200, 50);
+		
+		addButton("Image/reading.png", "Читать материалы", RedOxide, 0,60,320,335);
+		addButton("Image/word.png", "Учить слова", FaluRed, 320,60,230,335);
+		addButton("Image/dictionary.png", "Мои словари", CopperRose, 0,395,200,335);
+		addButton("Image/gramma.png", "Учить грамматику", PetiteOrchid, 200,395,350,335);
+	}
+	
+	public void addButton(String iconName, String buttonName, Color c, int x, int y, int w, int h){
+		ImageIcon icon = new ImageIcon(iconName);
+		Image img = icon.getImage() ;  
+		Image newimg = img.getScaledInstance( 80, 80,  java.awt.Image.SCALE_SMOOTH ) ;  
+		icon = new ImageIcon( newimg );
+		Font font = new Font("TimesRoman", Font.PLAIN, 18);
+		JButton button = new JButton(buttonName, icon);
+		button.setFont(font);
+		button.setBackground(c);
+		button.setForeground(Color.white);
+		add(button).setBounds(x,y,w,h);
+		
+		if(buttonName.equals("Читать материалы")){
+			button.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					try{
+						newframe = new ReadingMaterialsFrame();
+						newframe.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+						newframe.setVisible(true);
+					} catch (ClassNotFoundException e2) {
+						e2.printStackTrace();
+					}
+				}
+			});
+		}
+		
+		if(buttonName.equals("Мои словари")){
+			button.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					Dictionary frame = new Dictionary();
+					frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+					frame.setVisible(true);
+				}
+			});
+		}
+		
+		if(buttonName.equals("Учить слова")){
+			button.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					try {
+						LearnWordMenu frame = new LearnWordMenu();
+						frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+						frame.setVisible(true);
+					} catch (ClassNotFoundException e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(new Color(61,15,15));
+		g.fillRect(0, 0, 550, 60);
+	}
+}
---
+++ ReadingMaterialsFrame.java
@@ -0,0 +1,116 @@
+package nanoEnglish;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import javax.swing.*;
+
+public class ReadingMaterialsFrame extends JFrame{
+	public ReadingMaterialsFrame() throws ClassNotFoundException{
+		setSize(550,730);
+		ReadingMaterialsPanel panel = new ReadingMaterialsPanel(1);
+		JScrollPane scrollPane = new JScrollPane(panel);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		add(scrollPane).setBounds(0, 0, 550, 730);;
+	}
+}
+
+class ReadingMaterialsPanel extends JPanel{
+	String filename_level1[] = {"Text level 1/Christmas Cards", "Text level 1/Hit the Floor!", "Text level 1/Learning foreign languages", "Text level 1/The jackal and the elephant", "Text level 1/Traveling around the USA"};
+	String filename_level2[] = {};
+	String filename_level3[] = {};
+	int level, yt,yl;
+	public static Reading_Text newframe;
+
+	public ReadingMaterialsPanel(int level) throws ClassNotFoundException{
+		
+		setLayout(null);
+		setPreferredSize(new Dimension(550, 2840));
+		setBackground(new Color(123,31,31));
+		yt = 60;//20
+		yl = 20;
+		if (level==1){
+			for (int i=0; i<5; i++){
+				addText(filename_level1[i],yt,yl);
+				yt = yt+280;
+				yl = yl+280;
+			}
+		}
+		if (level==2){
+			for (int i=0; i<filename_level2.length; i++){
+				addText(filename_level2[i],yt,yl);
+				yt = yt+280;
+				yl = yl+280;
+			}
+		}
+		if (level==3){
+			for (int i=0; i<filename_level3.length; i++){
+				addText(filename_level3[i],yt,yl); 
+				yt = yt+280;
+				yl = yl+280;
+			}
+		}
+	}
+	
+	public void addText(String filename, int yt,int yl) throws ClassNotFoundException{
+		JLabel labelTitle = new JLabel(filename);
+		labelTitle.setHorizontalAlignment(JLabel.CENTER);
+		labelTitle.setFont(new Font("Verdana", Font.PLAIN, 20));
+		labelTitle.setBackground(Color.white);
+		add(labelTitle).setBounds(0,yl+10,550,40);
+		
+		String text = reading(filename+".txt");
+		JTextArea textArea = new JTextArea(text);
+        textArea.setFont(new Font("Dialog", Font.ROMAN_BASELINE, 14));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+		add(textArea).setBounds(45,yt+25,440,150);
+        
+		ImageIcon icon = new ImageIcon("Image/next.png");
+		Image img = icon.getImage() ;  
+		Image newimg = img.getScaledInstance( 80, 80,  java.awt.Image.SCALE_SMOOTH ) ;  
+		icon = new ImageIcon( newimg );
+		JButton button = new JButton(icon);
+		button.setBackground(new Color(210,149,149));
+		add(button).setBounds(460,yl+225,30,30);
+		
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				newframe = new Reading_Text(text, filename);
+				newframe.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+				newframe.setVisible(true);
+			}
+		});
+	}
+	
+	public String reading (String filename) throws ClassNotFoundException{
+		String text = "";
+		try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+            	text = text + line;
+                line = reader.readLine();
+            }
+            reader.close();
+        } 
+		catch (FileNotFoundException e) {} 
+		catch (IOException e) {}
+		return text;
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		int y = 20;
+		for (int i=0; i<10; i++){
+			g.setColor(Color.WHITE);
+			g.fillRect(20, y, 480, 260);
+			g.setColor(new Color(210,149,149));
+			g.fillRect(20, y+220, 480, 40);
+			y = y+280;
+		}
+	}
+}
